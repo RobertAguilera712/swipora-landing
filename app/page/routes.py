db01027e9ad5765473a51649ddb20aefd7365514
Roberto Aguilera <rc7.aguilera@gmail.com>
2026-04-20 16:33:43 +0000

    Meta tags added

--- app/page/routes.py
+++ app/page/routes.py
@@ -4,12 +4,12 @@ page = Blueprint("page", __name__, template_folder="../templates/page")
 
 @page.route("/", methods=["GET"])
 def index():
-    return render_template("index.jinja2")
+    return render_template("index.jinja2", title="Swipora | Aprende inglés de manera inteligente")
 
 @page.route("/privacy-policy/", methods=["GET"])
 def privacy():
-    return render_template("privacy-policy.jinja2")
+    return render_template("privacy-policy.jinja2", title="Swipora | Política de privacidad")
 
 @page.route("/terms-of-service/", methods=["GET"])
 def terms():
-    return render_template("terms-of-service.jinja2")
+    return render_template("terms-of-service.jinja2", title="Swipora | Términos de servicio")
